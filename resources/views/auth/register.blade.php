18b534204d4bd8d5d7a6906627087a5b04546f6b
perry <User@Tipzee-Aspire.(none)>
2018-03-12 20:22:38 +0800

    Layout change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,19 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-
-                <div class="panel-body">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title"><h1>{{ __('Register') }}</h1></div>
+                    <hr>
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
+                        <div class="form-group row {{ $errors->has('name') ? ' has-error' : '' }}">
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <label for="name"> {{__('Name')}} </label>
+                                <input id="name" 
+                                type="text" 
+                                class="form-control" 
+                                name="name" 
+                                value="{{ old('name') }}" 
+                                required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -23,13 +27,14 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <label for="email"> {{__('Email')}} </label>
+                                <input id="email" 
+                                type="email" 
+                                class="form-control" 
+                                name="email" 
+                                value="{{ old('email') }}" 
+                                required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -39,10 +44,9 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
+                        <div class="form-group row">
                             <div class="col-md-6">
+                                <label for="password"> {{__('Password')}} </label>
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -51,20 +55,20 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <label for="password-confirm"> {{__('Confirm Password')}} </label>
+                                <input id="password-confirm" 
+                                    type="password" 
+                                    class="form-control" 
+                                    name="password_confirmation" 
+                                    required>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="form-group row text-right">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
